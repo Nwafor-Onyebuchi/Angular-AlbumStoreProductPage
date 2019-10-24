ea1d2be4d727943fd8edf2ef2aac661a593eadf0
Onyebuchi-Nwafor <buchilazarus4@gmail.com>
2019-10-24 07:22:28 +0100

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class ProductService {
+  private _album = "../assets/album.json";
+  constructor(private _http: Http) {}
+
+  getAlbum(id: number) {
+    return this._http.get(this._album).map(response => response.json());
+  }
+}
